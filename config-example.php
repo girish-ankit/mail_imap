f3a1cb9df72ae89606b9b8b233c57789be4f8698
Ankit Kumar <GIRISH.ANKIT@gmail.com>
2019-09-26 06:53:30 -0400

    config example

---
+++ config-example.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+    'database' => [
+        'host' => 'localhost',
+        'name' => '',
+        'user' => '',
+        'pass' => ''
+    ],
+    'mail' => [
+        'hostname' => 'imap.gmail.com',
+        'port' => '993',
+        'username' => '',
+        'passsword' => ''
+    ]
+];
